39f2bd6a40732f19c3c04a29b00e4bd7423dee8e
Mladen Raykov <raykovmladen@gmail.com>
2025-07-17 19:01:31 +0300

    change endpoint

--- assets/js/api-connect.js
+++ assets/js/api-connect.js
@@ -20,7 +20,7 @@ document.getElementById('domainForm').addEventListener('submit', async function(
 
     try {
         // Use your deployed Vercel API URL
-        const apiUrl = 'https://blocklang-5cz9b2su1-madjis-projects.vercel.app/api/check';
+        const apiUrl = 'https://blocklang-5cz9b2su1-madjis-projects.vercel.app/api/domain-checker';
         const response = await fetch(apiUrl, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
